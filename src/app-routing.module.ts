c4d7dd0a0c1377224dd9f47b55347f6f35a14bdf
SourabhDeepBhatnagar <sourabh.bhatnagar84@gmail.com>
2024-01-21 14:17:50 +0530

    Deployment fix

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './app/home.component';
-import { ProductsDetailComponent } from './app/shared/products/product.component';
+import { ProductComponent } from './app/shared/products/product.component';
 import { ProductsComponent } from './app/shared/products/products.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'detail/:id', component: ProductsDetailComponent },
+  { path: 'detail/:id', component: ProductComponent },
   { path: 'product', component: ProductsComponent }
 ];
 
